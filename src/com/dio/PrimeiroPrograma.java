b4b6abf1b6a8fce04d201df297e9e851dcfa794d
Yane <yaneaguiar@hotmail.com>
2022-03-16 09:44:46 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		Gato gato = new Gato();
+		
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		
+		/* int a = 2;
+		int b = 3;
+		System.out.println("Hello World " + (a+b));
+
+	}*/
+		
+
+}
+
+ 
